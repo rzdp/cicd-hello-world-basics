cc302519e56063ea2cea01bb9d7f00f83d118c4a
xrriz <rriz.pena@gmail.com>
2020-07-26 19:17:38 +0800

    Initial commit

---
+++ src/main/java/com/rzdp/cicdhelloworldbasics/CicdHelloWorldBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.rzdp.cicdhelloworldbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdHelloWorldBasicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdHelloWorldBasicsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rzdp/cicdhelloworldbasics/CicdHelloWorldBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rzdp.cicdhelloworldbasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdHelloWorldBasicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
