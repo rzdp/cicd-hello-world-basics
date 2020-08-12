7f18e6f171e685f9fb3305d6364b9bbaecc21507
xrriz <rriz.pena@gmail.com>
2020-08-12 15:24:58 +0800

    Update CicdHelloWorldBasicsApplication.java

--- src/main/java/com/rzdp/cicdhelloworldbasics/CicdHelloWorldBasicsApplication.java
+++ src/main/java/com/rzdp/cicdhelloworldbasics/CicdHelloWorldBasicsApplication.java
@@ -2,12 +2,19 @@ package com.rzdp.cicdhelloworldbasics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CicdHelloWorldBasicsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CicdHelloWorldBasicsApplication.class, args);
     }
 
+    @GetMapping("/hello-world")
+    public String sayHello() {
+        return "Hello World";
+    }
 }
